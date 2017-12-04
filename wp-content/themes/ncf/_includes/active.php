a86728c498df31893841d7ba4a0eaa120d215675
Kevin McCormick <admin@nc-f.org>
2017-12-04 12:53:10 -0800

    start static conversion

---
+++ wp-content/themes/ncf/_includes/active.php
@@ -0,0 +1,8 @@
+<!-- begin active.php -->
+<script>
+jQuery( document ).ready( function( $ ) {
+	$( '#crime' ).addClass( 'active' );
+	$( '#ec' ).addClass( 'active' );
+});
+</script>
+<!-- end active.php -->
