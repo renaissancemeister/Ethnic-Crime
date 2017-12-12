8fc872d08d6e9458501ebe263569834929227a3b
Kevin McCormick <admin@nc-f.org>
2017-12-12 15:44:34 -0800

    old WP file cleanup

--- wp-content/themes/ncf/_includes/active.php
+++
@@ -1,8 +0,0 @@
-<!-- begin active.php -->
-<script>
-jQuery( document ).ready( function( $ ) {
-	$( '#crime' ).addClass( 'active' );
-	$( '#ec' ).addClass( 'active' );
-});
-</script>
-<!-- end active.php -->
